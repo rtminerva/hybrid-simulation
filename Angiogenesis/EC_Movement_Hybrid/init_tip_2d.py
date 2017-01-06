9ce7a3661c17daff2fcf3f009676823292c5bd27
suzuki_lab <suzuki_lab@suzuki_cell02>
2017-01-06 17:59:04 +0900

    update

--- Angiogenesis/EC_Movement_Hybrid/init_tip_2d.py
+++ Angiogenesis/EC_Movement_Hybrid/init_tip_2d.py
@@ -6,11 +6,66 @@ def rec_1_tip(coef,set,sol): #2.1.2.(1)
     y = set['Ny']/2 
     if y % 2 == 0:
         y += 1
-    sol['matrix_tip'].append([(5,y)])
-    sol['n'][5,y] = 1
+    sol['matrix_tip'].append([(10,y)])
+    sol['n'][10,y] = 1
     sol['b'][3,y] = 1
+    #sol['b'][5,y] = 1
+    #sol['b'][7,y] = 1
     sol['list_tip_movement'].append('start') #movement tip
     sol['life_time_tip'].append(0) #lifetime
+    return sol
+
+def rec_5_tip(coef,set,sol): #2.1.2.(2)
+    '''Tip 0'''
+    y = set['Ny']/6
+    if y % 2 == 0:
+        y += 1
+    sol['matrix_tip'].append([(10,y-24)])
+    sol['n'][10,y-24] = 1
+    sol['b'][5,y-24] = 1
+    sol['list_tip_movement'].append('start') #movement tip
+    sol['life_time_tip'].append(0) #lifetime
+    
+    '''TIP 1'''
+    y1 = 2*y
+    if y1 % 2 == 0:
+        y1 += 1
+    sol['matrix_tip'].append([(10,y1-14)])
+    sol['n'][10,y1-14] = 1
+    sol['b'][5,y1-14] = 1
+    sol['list_tip_movement'].append('start') #movement tip
+    sol['life_time_tip'].append(0) #lifetime
+    
+    '''TIP 2'''
+    y2 = 3*y
+    if y2 % 2 == 0:
+        y2 += 1
+    sol['matrix_tip'].append([(10,y2)])
+    sol['n'][10,y2] = 1
+    sol['b'][5,y2] = 1
+    sol['list_tip_movement'].append('start') #movement tip
+    sol['life_time_tip'].append(0) #lifetime
+    
+    '''TIP 3'''
+    y2 = 4*y
+    if y2 % 2 == 0:
+        y2 += 1
+    sol['matrix_tip'].append([(10,y2+14)])
+    sol['n'][10,y2+14] = 1
+    sol['b'][5,y2+14] = 1
+    sol['list_tip_movement'].append('start') #movement tip
+    sol['life_time_tip'].append(0) #lifetime
+    
+    '''TIP 4'''
+    y2 = 5*y
+    if y2 % 2 == 0:
+        y2 += 1
+    sol['matrix_tip'].append([(10,y2+24)])
+    sol['n'][10,y2+24] = 1
+    sol['b'][5,y2+24] = 1
+    sol['list_tip_movement'].append('start') #movement tip
+    sol['life_time_tip'].append(0) #lifetime
+    
     return sol
 
 def init_tip_2d_(coef,set,sol):
@@ -25,8 +80,8 @@ def init_tip_2d_(coef,set,sol):
     
     sol['sp_stop'] = []
     sol['tip_cell'] = []
-    sol = rec_1_tip(coef,set,sol) #2.1.2.(1)
-#     sol = rec_5_tip(coef,set,sol) #2.1.2.(2)
+#     sol = rec_1_tip(coef,set,sol) #2.1.2.(1)
+    sol = rec_5_tip(coef,set,sol) #2.1.2.(2)
         
     '''Identifying Tip Cell'''
     for e,ti in enumerate(sol['matrix_tip']):
