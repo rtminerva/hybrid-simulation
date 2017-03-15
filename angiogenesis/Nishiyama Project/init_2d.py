288396c5a53faefb7c275560df95c55ef140eb19
suzuki_lab <suzuki_lab@suzuki_cell02>
2017-03-15 17:35:30 +0900

    g

--- angiogenesis/Nishiyama Project/init_2d.py
+++ angiogenesis/Nishiyama Project/init_2d.py
@@ -4,10 +4,10 @@ import math as m
 def c_prof_1(coef,set,sol): #2.1.1.(1)
     for y in range(0,set['Ny']+1,2):
         for x in range(0,set['Nx']+1,2):
-            sol['c'][x,y] = 0.5
-            sol['c_o'][x,y] = 0.5
-#             sol['c'][x,y] = numpy.exp(-(1-x*set['Hh'])**2/0.45)
-#             sol['c_o'][x,y] = numpy.exp(-(1-x*set['Hh'])**2/0.45)
+#             sol['c'][x,y] = 0.5
+#             sol['c_o'][x,y] = 0.5
+            sol['c'][x,y] = numpy.exp(-(1-x*set['Hh'])**2/0.45)
+            sol['c_o'][x,y] = numpy.exp(-(1-x*set['Hh'])**2/0.45)
     return sol
     
 def c_prof_2(coef,set,sol): #2.1.1.(2)
