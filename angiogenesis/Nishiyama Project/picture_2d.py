bc92f6e6a8b9f113ff7edda3260f69f5cfd5d328
rtminerva <rtminerva@MinervaPC.Suzuki.Lab>
2017-03-30 18:15:42 +0900

    f

--- angiogenesis/Nishiyama Project/picture_2d.py
+++ angiogenesis/Nishiyama Project/picture_2d.py
@@ -6,7 +6,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 def pic_2d(coef,set,sol):           
     '''EC'''
-    fig = plt.figure()
+    fig = plt.figure(1)
     plt.title('%s%f' % ('t=',set['t']))
     ax = fig.add_subplot(111)
     for i in range(0,len(sol['matrix_tip'])):
@@ -91,57 +91,56 @@ def pic_2d(coef,set,sol):
         plt.savefig("%s.png" % flag)
         plt.close()
          
-#         '''Different VEGF'''
-#         fig3 = plt.figure(11)
-#         plt.title('%s%f' % ('VEGF at t=',set['t']))
-#         plt.xlabel('X')
-#         plt.ylabel('Y')           
-#         cn_sol = numpy.ma.masked_array(cn_sol, cn_sol < 0.0001)
-#         plt.pcolormesh(y_sub_axis, x_sub_axis, cn_sol, vmin = 0, vmax = 1, cmap = 'winter', shading = 'gouraud')
-#         x_p = []
-#         y_p = []
-#         for tip in sol['tip_cell']:
-#             x_p.append(tip[0]*set['Hh'])
-#             y_p.append(tip[1]*set['Hh'])
-#         ax.scatter(x_p, y_p, marker = 'o', s = 10, color ='r')
-#         sol['VEGF1'] +=1  
-#         flag = 'VEGF1=%s' % str(sol['VEGF1']) 
-#         plt.colorbar()
-#         plt.savefig("%s.png" % flag)
-#         plt.close()
+        '''Different VEGF'''
+        fig3 = plt.figure(11)
+        plt.title('%s%f' % ('VEGF at t=',set['t']))
+        plt.xlabel('X')
+        plt.ylabel('Y')           
+        cn_sol = numpy.ma.masked_array(cn_sol, cn_sol < 0.0001)
+        plt.pcolormesh(y_sub_axis, x_sub_axis, cn_sol, vmin = 0, vmax = 1, cmap = 'winter', shading = 'gouraud')
+        x_p = []
+        y_p = []
+        for tip in sol['tip_cell']:
+            x_p.append(tip[0]*set['Hh'])
+            y_p.append(tip[1]*set['Hh'])
+        ax.scatter(x_p, y_p, marker = 'o', s = 10, color ='r')
+        sol['VEGF1'] +=1  
+        flag = 'VEGF1=%s' % str(sol['VEGF1']) 
+        plt.colorbar()
+        plt.savefig("%s.png" % flag)
+        plt.close()
         
-        
-        
-        
-        
-        
-#     
-#     '''Merge_fn'''
-#     fig12 = plt.figure()
-#     plt.title('%s%f' % ('merge of ECM and Vessel at t=',set['t']))
-# #         plt.xlim(set['Hh'],coef['X']-set['Hh'])
-# #         plt.ylim(set['Hh'],coef['Y']-set['Hh'])
-#     ax = fig12.add_subplot(111)
-#     x_p = []
-#     y_p = []
-#     for tip in sol['tip_cell']:
-#         x_p.append(tip[0]*set['Hh'])
-#         y_p.append(tip[1]*set['Hh'])
-#     ax.scatter(x_p, y_p, marker = 'o', s = 10, color ='r')
-#     for i in range(0,len(sol['matrix_tip'])):
-#         x_p = []
-#         y_p = []
-#         for j in range(0,len(sol['matrix_tip'][i])):
-#             x_p.append(sol['matrix_tip'][i][j][0]*set['Hh'])
-#             y_p.append(sol['matrix_tip'][i][j][1]*set['Hh'])
-#         globals()['plo%s' % i] = ax.plot(x_p, y_p, 'c', color ='k')
-#     f_sol = numpy.ma.masked_array(f_sol, f_sol < 0.0001)#-.5)
-#     plt.pcolormesh(y_sub_axis, x_sub_axis, f_sol, vmin = 0.499, vmax = 0.504, cmap="BuPu")
-#     sol['Merge_fn'] +=1
-#     flag = 'Merge_fn=%s' % str(sol['Merge_fn']) 
+    '''Merge_fn'''
+    fig12 = plt.figure()
+    plt.title('%s%f' % ('merge of ECM and Vessel at t=',set['t']))
+#         plt.xlim(set['Hh'],coef['X']-set['Hh'])
+#         plt.ylim(set['Hh'],coef['Y']-set['Hh'])
+    ax = fig12.add_subplot(111)
+    x_p = []
+    y_p = []
+    for tip in sol['tip_cell']:
+        x_p.append(tip[0]*set['Hh'])
+        y_p.append(tip[1]*set['Hh'])
+    ax.scatter(x_p, y_p, marker = 'o', s = 10, color ='r')
+  
+    for i in range(0,len(sol['matrix_tip'])):
+        x_p = []
+        y_p = []
+        for j in range(0,len(sol['matrix_tip'][i])):
+            x_p.append(sol['matrix_tip'][i][j][0]*set['Hh'])
+            y_p.append(sol['matrix_tip'][i][j][1]*set['Hh'])
+        globals()['plo%s' % i] = ax.plot(x_p, y_p, 'c', color ='k')
+  
+    f_sol = numpy.ma.masked_array(f_sol, f_sol < 0.0001)#-.5)
+    ax.pcolormesh(y_sub_axis, x_sub_axis, f_sol, vmin = 0.499, vmax = 0.504, cmap="BuPu")
+    plt.xlim((set['Hh'],coef['X']-set['Hh']))
+    plt.ylim((set['Hh'],coef['Y']-set['Hh']))
+  
+    sol['Merge_fn'] +=1
+    flag = 'Merge_fn=%s' % str(sol['Merge_fn']) 
 #     plt.colorbar()
-#     plt.savefig("%s.png" % flag)
-#     plt.close()
+    plt.savefig("%s.png" % flag)
+    plt.close()
 #     
 #     '''Merge_cn'''
 #     fig13 = plt.figure()
