cea4042d444e03ec805e7ff2730c334c78ca48f8
rtminerva <rtminerva@MinervaPC.Suzuki.Lab>
2017-04-03 10:42:44 +0900

    fig

--- angiogenesis/Nishiyama Project/picture_2d.py
+++ angiogenesis/Nishiyama Project/picture_2d.py
@@ -7,7 +7,7 @@ from mpl_toolkits.mplot3d import Axes3D
 def pic_2d(coef,set,sol):           
     '''EC'''
     fig = plt.figure(1)
-    plt.title('%s%f' % ('t=',set['t']))
+    plt.title('%s%f' % ('Vessel Growth at t=',set['t']))
     plt.xlabel('X')
     plt.ylabel('Y') 
     ax = fig.add_subplot(111)
@@ -85,61 +85,24 @@ def pic_2d(coef,set,sol):
          
         '''Different VEGF'''
         fig3 = plt.figure(4)
-        plt.title('%s%f' % ('VEGF at t=',set['t']))
+        plt.title('%s%f' % ('VEGF Distribution (Normalised) at t=',set['t']))
         plt.xlabel('X')
         plt.ylabel('Y')           
         cn_sol = numpy.ma.masked_array(cn_sol, cn_sol < 0.0001)
         plt.pcolormesh(y_sub_axis, x_sub_axis, cn_sol, vmin = 0, vmax = 1, cmap = 'winter', shading = 'gouraud')
         sol['VEGF1'] +=1  
-        flag = 'VEGF1=%s' % str(sol['VEGF1']) 
+        flag = 'VEGF_norm=%s' % str(sol['VEGF1']) 
         plt.colorbar()
         plt.savefig("%s.png" % flag)
         plt.close()
-        
-    '''MERGE_fn'''
-    fig12 = plt.figure(5)
-    plt.title('%s%f' % ('merge of ECM and Vessel at t=',set['t']))
-    ax = fig12.add_subplot(111)
-    '''ECM'''
-    f_sol = numpy.ma.masked_array(f_sol, f_sol < 0.0001)#-.5)
-    ax.pcolormesh(y_sub_axis, x_sub_axis, f_sol, vmin = 0.499, vmax = 0.504, cmap="BuPu")
-    '''Tip Cell'''
-    x_p = []
-    y_p = []
-    for tip in sol['tip_cell']:
-        x_p.append(tip[0]*set['Hh'])
-        y_p.append(tip[1]*set['Hh'])
-    ax.scatter(x_p, y_p, marker = 'o', s = 10, color ='r')
-    '''Backward Marker'''
-    if len(sol['backward_list']) > 0:
-        x_pp = []
-        y_pp = []
-        for tip in sol['backward_list']:
-            x_pp.append(tip[0]*set['Hh'])
-            y_pp.append(tip[1]*set['Hh'])
-        ax.scatter(x_pp, y_pp, marker = '^', s = 10, color ='c')
-    '''Vessel Growth'''
-    for i in range(0,len(sol['matrix_tip'])):
-        x_p = []
-        y_p = []
-        for j in range(0,len(sol['matrix_tip'][i])):
-            x_p.append(sol['matrix_tip'][i][j][0]*set['Hh'])
-            y_p.append(sol['matrix_tip'][i][j][1]*set['Hh'])
-        globals()['plo%s' % i] = ax.plot(x_p, y_p, 'c', color ='k')
-    plt.xlim((set['Hh'],coef['X']-set['Hh']))
-    plt.ylim((set['Hh'],coef['Y']-set['Hh']))
-    sol['Merge_fn'] +=1
-    flag = 'Merge_fn=%s' % str(sol['Merge_fn']) 
-    plt.savefig("%s.png" % flag)
-    plt.close()
     
     '''MERGE_cn'''
     fig13 = plt.figure(6)
-    plt.title('%s%f' % ('merge of ECM and Vessel at t=',set['t']))
+    plt.title('%s%f' % ('VEGF and Vessel t=',set['t']))
     ax = fig13.add_subplot(111)
     '''ecm'''
     c_sol = numpy.ma.masked_array(c_sol, c_sol < 0.0001)#-.5)
-    ax.pcolormesh(y_sub_axis, x_sub_axis, c_sol, vmin = 0.499, vmax = 0.504, cmap="BuPu")
+    ax.pcolormesh(y_sub_axis, x_sub_axis, c_sol, vmin = 0, vmax = 1, cmap="Blues", shading = 'gouraud')
     '''tip cell'''
     x_p = []
     y_p = []
@@ -170,14 +133,51 @@ def pic_2d(coef,set,sol):
     flag = 'Merge_cn=%s' % str(sol['Merge_fn']) 
     plt.savefig("%s.png" % flag)
     plt.close()
+    
+    '''MERGE_fn'''
+    fig12 = plt.figure(5)
+    plt.title('%s%f' % ('ECM and Vessel at t=',set['t']))
+    ax = fig12.add_subplot(111)
+    '''ECM'''
+    f_sol = numpy.ma.masked_array(f_sol, f_sol < 0.0001)#-.5)
+    ax.pcolormesh(y_sub_axis, x_sub_axis, f_sol, vmin = 0.499, vmax = 0.504, cmap="BuPu", shading = 'gouraud')
+    '''Tip Cell'''
+    x_p = []
+    y_p = []
+    for tip in sol['tip_cell']:
+        x_p.append(tip[0]*set['Hh'])
+        y_p.append(tip[1]*set['Hh'])
+    ax.scatter(x_p, y_p, marker = 'o', s = 10, color ='r')
+    '''Backward Marker'''
+    if len(sol['backward_list']) > 0:
+        x_pp = []
+        y_pp = []
+        for tip in sol['backward_list']:
+            x_pp.append(tip[0]*set['Hh'])
+            y_pp.append(tip[1]*set['Hh'])
+        ax.scatter(x_pp, y_pp, marker = '^', s = 10, color ='c')
+    '''Vessel Growth'''
+    for i in range(0,len(sol['matrix_tip'])):
+        x_p = []
+        y_p = []
+        for j in range(0,len(sol['matrix_tip'][i])):
+            x_p.append(sol['matrix_tip'][i][j][0]*set['Hh'])
+            y_p.append(sol['matrix_tip'][i][j][1]*set['Hh'])
+        globals()['plo%s' % i] = ax.plot(x_p, y_p, 'c', color ='k')
+    plt.xlim((set['Hh'],coef['X']-set['Hh']))
+    plt.ylim((set['Hh'],coef['Y']-set['Hh']))
+    sol['Merge_fn'] +=1
+    flag = 'Merge_fn=%s' % str(sol['Merge_fn']) 
+    plt.savefig("%s.png" % flag)
+    plt.close()
 
     '''MERGE_cnd'''
     fig14 = plt.figure(7)
-    plt.title('%s%f' % ('merge of ECM and Vessel at t=',set['t']))
+    plt.title('%s%f' % ('VEGF (Normalised) and Vessel at t=',set['t']))
     ax = fig14.add_subplot(111)
     '''ecm'''
     cn_sol = numpy.ma.masked_array(cn_sol, cn_sol < 0.0001)#-.5)
-    ax.pcolormesh(y_sub_axis, x_sub_axis, cn_sol, vmin = 0.499, vmax = 0.504, cmap="BuPu")
+    ax.pcolormesh(y_sub_axis, x_sub_axis, cn_sol, vmin = 0, vmax = 1, cmap = 'winter', shading = 'gouraud')
     '''tip cell'''
     x_p = []
     y_p = []
@@ -204,7 +204,7 @@ def pic_2d(coef,set,sol):
     plt.xlim((set['Hh'],coef['X']-set['Hh']))
     plt.ylim((set['Hh'],coef['Y']-set['Hh']))
     sol['Merge_cnd'] +=1
-    flag = 'Merge_cnd=%s' % str(sol['Merge_cnd']) 
+    flag = 'Merge_cn_norm=%s' % str(sol['Merge_cnd']) 
     plt.savefig("%s.png" % flag)
     plt.close()  
 
