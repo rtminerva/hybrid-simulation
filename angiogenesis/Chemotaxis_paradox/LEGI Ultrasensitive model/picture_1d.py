1236e9c8929c9cf75561916821277129e5ee1e1c
Suzuki-lab <Suzuki-lab@windowsHPDM>
2018-08-07 19:01:13 +0900

    fff

--- angiogenesis/Chemotaxis_paradox/LEGI Ultrasensitive model/picture_1d.py
+++ angiogenesis/Chemotaxis_paradox/LEGI Ultrasensitive model/picture_1d.py
@@ -10,7 +10,7 @@ from dask.array.creation import linspace
 
 def pic_1d(coef,set,sol):
     script_dir = os.path.dirname(__file__)
-    results_dir = os.path.join(script_dir, 'basic/')
+    results_dir = os.path.join(script_dir, 'ultrasensitive/')
     
     if not os.path.isdir(results_dir):
         os.makedirs(results_dir)
